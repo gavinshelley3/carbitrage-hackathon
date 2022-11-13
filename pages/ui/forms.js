99aef47c39b70de3649f6e645522869bf6fb70c9
Gavin Shelley <69445267+gavinshelley3@users.noreply.github.com>
2022-11-12 18:23:41 -0700

    Car Sort (#2)

--- pages/ui/forms.js
+++ pages/ui/forms.js
@@ -1,8 +1,9 @@
-import {
-  useState
-}
-from 'react'
-import getScores from '../utils';
+import React, { useState } from 'react';
+// import getScores from '../ui/utils';
+import { collection, getDocs } from "firebase/firestore";
+import db from "../../firebase/config";
+
+
 import {
   Card,
   Row,
@@ -17,25 +18,84 @@ import {
   FormText,
 } from 'reactstrap';
 
+
+
 const Forms = () => {
   const [data, setData] = useState(null);
   const [name, setName] = useState(null);
+  // const getScores(inputArray);
+
+  const [inputArray, setInputArray] = useState({
+    MakeWeight : 3,
+    MakeText : "",
+    ModelWeight : 3,
+    ModelText : "",
+    YearWeight : 3,
+    YearText : 1,
+    PriceWeight : 3,
+    PriceText : 1,
+    MileageWeight : 3,
+    MileageText : 1,
+    ColorWeight : 3,
+    ColorText : "",
+  });
+
+  async function getScores() {
+
+    const carsCollection = collection(db, "cars");
+    const carsSnapshot = await getDocs(carsCollection);
+  
+    const carsList = carsSnapshot.docs.map((doc) => doc.data());
+  
+    const carScores = [];
 
 
-const inputArray = {
-  MakeWeight : "",
-  MakeText : "",
-  ModelWeight : "",
-  ModelText : "",
-  YearWeight : "",
-  YearText : "",
-  PriceWeight : "",
-  PriceText : "",
-  MileageWeight : "",
-  MileageText : "",
-  ColorWeight : "",
-  ColorText : "",
-};
+
+    carsList.forEach((item) => {
+      const scores = {
+        MakeScore : 0,
+        ModelScore : 0,
+        YearScore : 0,
+        PriceScore : 0,
+        MileageScore : 0,
+        ColorScore : 0,
+      };
+
+      // console.log(inputArray)
+      
+      {item.make && item.make.toLowerCase() === inputArray.MakeText.toLowerCase() ? scores.MakeScore = 0 : scores.MakeScore = 5}
+      {item.model && item.model.toLowerCase() === inputArray.ModelText.toLowerCase() ? scores.ModelScore = 0 : scores.ModelScore = 10}
+      {item.paint && item.paint[0].toLowerCase() === inputArray.ColorText.toLowerCase() ? scores.ColorScore = 0 : scores.ColorScore = 1}
+  
+        
+        const yearDiff = (item.makeYear - inputArray.YearText);
+        scores.YearScore = yearDiff * inputArray.YearWeight * 2;
+  
+        const priceDiff = (item.price - inputArray.PriceText);
+        scores.PriceScore = priceDiff/inputArray.PriceText * inputArray.PriceWeight * 10;
+  
+        const mileageDiff = (item.mileage - inputArray.MileageText);
+        scores.MileageScore = mileageDiff/inputArray.MileageText * inputArray.MileageWeight * 5;
+  
+        const totalScore = scores.MakeScore + scores.ModelScore + scores.YearScore + scores.PriceScore + scores.MileageScore + scores.ColorScore;
+  
+  
+        // const itemScore = scores.MakeScore + scores.ModelScore + scores.YearScore + scores.PriceScore + scores.MileageScore + scores.ColorScore;
+        const itemScore = 100 - totalScore;
+        // console.log(scores);
+        // console.log("Make:[" + item.make + "] Model:[" + item.model + "] Year:[" + item.makeYear + "] $" + item.price + " Miles:[" + item.mileage + "]")
+        // console.log("Score:" + itemScore);
+        
+        item.score = itemScore;      
+        carScores.push(item);
+    });
+    carScores.sort((a,b) => b.score - a.score)
+    console.log(carScores);
+    // return carScores;
+    };
+
+
+
 
   return (
     <Row>
@@ -52,8 +112,8 @@ const inputArray = {
             <Form>
             <FormGroup>
                 <Label for="MakeWeight">MakeWeight</Label>
-                <Input id="MakeWeight" name="select" type="select" onChange={e => inputArray.MakeWeight = (e.target.value)}>
-                  <option value='1'>1</option>
+                <Input id="MakeWeight" name="MakeWeight" type="select" onChange={e => inputArray.MakeWeight = (e.target.value)}>
+                  <option value="1">1</option>
                   <option value='2'>2</option>
                   <option value='3'>3</option>
                   <option value='4'>4</option>
@@ -62,12 +122,12 @@ const inputArray = {
               </FormGroup>
               <FormGroup>
                 <Label for="MakeText">MakeText</Label>
-                <Input id="MakeText" name="text" type="textarea" onChange={e => inputArray.MakeText = (e.target.value)}/>
+                <Input id="MakeText" name="MakeText" type="textarea" onChange={(e) => {setInputArray(inputArray => ({...inputArray, ...{"MakeText": e.target.value}}))}}/>
               </FormGroup>
               <FormGroup>
                 <Label for="ModelWeight">ModelWeight</Label>
-                <Input id="ModelWeight" name="select" type="select" onChange={e => inputArray.ModelWeight = (e.target.value)}>
-                <option value='1'>1</option>
+                <Input id="ModelWeight" name="ModelWeight" type="select" onChange={e => inputArray.ModelWeight = (e.target.value)}>
+                  <option value='1'>1</option>
                   <option value='2'>2</option>
                   <option value='3'>3</option>
                   <option value='4'>4</option>
@@ -76,12 +136,12 @@ const inputArray = {
               </FormGroup>
               <FormGroup>
                 <Label for="ModelText">ModelText</Label>
-                <Input id="ModelText" name="text" type="textarea"  onChange={e => inputArray.ModelText = (e.target.value)}/>
+                <Input id="ModelText" name="ModelText" type="textarea"  onChange={e => inputArray.ModelText = (e.target.value)}/>
               </FormGroup>
               <FormGroup>
                 <Label for="MileageWeight">MileageWeight</Label>
-                <Input id="MileageWeight" name="select" type="select" onChange={e => inputArray.MileageWeight = (e.target.value)}>
-                <option value='1'>1</option>
+                <Input id="MileageWeight" name="MileageWeight" type="select" onChange={e => inputArray.MileageWeight = (e.target.value)}>
+                  <option value='1'>1</option>
                   <option value='2'>2</option>
                   <option value='3'>3</option>
                   <option value='4'>4</option>
@@ -90,12 +150,12 @@ const inputArray = {
               </FormGroup>
               <FormGroup>
                 <Label for="MileageText">MileageText</Label>
-                <Input id="MileageText" name="text" type="textarea"  onChange={e => inputArray.MileageText = (e.target.value)}/>
+                <Input id="MileageText" name="MileageText" type="textarea"  onChange={e => inputArray.MileageText = (e.target.value)}/>
               </FormGroup>
               <FormGroup>
                 <Label for="YearWeight">YearWeight</Label>
-                <Input id="YearWeight" name="select" type="select" onChange={e => inputArray.YearWeight = (e.target.value)}>
-                <option value='1'>1</option>
+                <Input id="YearWeight" name="YearWeight" type="select" onChange={e => inputArray.YearWeight = (e.target.value)}>
+                  <option value='1'>1</option>
                   <option value='2'>2</option>
                   <option value='3'>3</option>
                   <option value='4'>4</option>
@@ -104,12 +164,12 @@ const inputArray = {
               </FormGroup>
               <FormGroup>
                 <Label for="YearText">YearText</Label>
-                <Input id="YearText" name="text" type="textarea"  onChange={e => inputArray.YearText = (e.target.value)}/>
+                <Input id="YearText" name="YearText" type="textarea"  onChange={e => inputArray.YearText = (e.target.value)}/>
               </FormGroup>
               <FormGroup>
                 <Label for="PriceWeight">PriceWeight</Label>
-                <Input id="PriceWeight" multiple name="selectMulti" type="select" onChange={e => inputArray.PriceWeight = (e.target.value)}>
-                <option value='1'>1</option>
+                <Input id="PriceWeight" name="PriceWeight" type="select" onChange={e => inputArray.PriceWeight = (e.target.value)}>
+                  <option value='1'>1</option>
                   <option value='2'>2</option>
                   <option value='3'>3</option>
                   <option value='4'>4</option>
@@ -118,12 +178,12 @@ const inputArray = {
               </FormGroup>
               <FormGroup>
                 <Label for="PriceText">PriceText</Label>
-                <Input id="PriceText" name="text" type="textarea"  onChange={e => inputArray.PriceText = (e.target.value)}/>
+                <Input id="PriceText" name="PriceText" type="textarea"  onChange={e => inputArray.PriceText = (e.target.value)}/>
               </FormGroup>
               <FormGroup>
                 <Label for="ColorWeight">ColorWeight</Label>
-                <Input id="ColorWeight" name="select" type="select" onChange={e => inputArray.ColorWeight = (e.target.value)}>
-                <option value='1'>1</option>
+                <Input id="ColorWeight" name="ColorWeight" type="select" onChange={e => inputArray.ColorWeight = (e.target.value)}>
+                  <option value='1'>1</option>
                   <option value='2'>2</option>
                   <option value='3'>3</option>
                   <option value='4'>4</option>
@@ -132,10 +192,10 @@ const inputArray = {
               </FormGroup>
               <FormGroup>
                 <Label for="ColorText">ColorText</Label>
-                <Input id="ColorText" name="text" type="textarea"  onChange={e => inputArray.ColorText = (e.target.value)}/>
+                <Input id="ColorText" name="ColorText" type="textarea"  onChange={e => inputArray.ColorText = (e.target.value)}/>
               </FormGroup>
               <Button 
-              onClick={ getScores(inputArray)}
+                onClick={getScores}
               >Submit</Button>
             </Form>
           </CardBody>
@@ -144,5 +204,7 @@ const inputArray = {
     </Row>
   );
 };
+
+
 
 export default Forms;
